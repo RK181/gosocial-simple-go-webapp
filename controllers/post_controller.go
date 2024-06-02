19bb5e642bf1eea95e31f0c15d377396bb7459ac
Robert Kazaryan <rk30@gcloud.ua.es>
2024-06-02 22:09:47 +0200

    feat: Add "ForSubcribers" field to post creation and update forms

--- controllers/post_controller.go
+++ controllers/post_controller.go
@@ -8,8 +8,9 @@ import (
 )
 
 type PostFormError struct {
-	Title   string
-	Content string
+	Title         string
+	Content       string
+	ForSubcribers bool
 }
 
 type PostController struct{}
@@ -22,19 +23,21 @@ func (c *PostController) CreatePostPost(w http.ResponseWriter, r *http.Request) 
 	// Obtenemos los datos del formulario
 	title := r.FormValue("title")
 	content := r.FormValue("content")
+	forSubcribers := r.FormValue("forSubcribers")
 
 	data := make(map[string]interface{})
 	// TO-DO: Validar los datos del formulario
 
 	user := r.Context().Value(shared.AUTH_USER).(models.User)
-	post := models.Post{UserID: user.ID, Title: title, Content: content}
+	post := models.Post{UserID: user.ID, Title: title, Content: content, ForSubcribers: forSubcribers == "on"}
 	err := post.CreatePost()
 	if err != nil {
 		data["Title"] = title
 		data["Content"] = content
 		data["Errors"] = PostFormError{
-			Title:   "Check if the title is correct",
-			Content: "Check if the content is correct",
+			Title:         "Check if the title is correct",
+			Content:       "Check if the content is correct",
+			ForSubcribers: forSubcribers == "on",
 		}
 		tmpl := shared.Templates["postCreateUpdateForm.html"]
 		tmpl.Execute(w, data)
@@ -56,7 +59,6 @@ func (c *PostController) UpdatePostGet(w http.ResponseWriter, r *http.Request) {
 
 	if post.UserID != user.ID {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		//return
 	}
 
 	data := make(map[string]interface{})
@@ -65,7 +67,7 @@ func (c *PostController) UpdatePostGet(w http.ResponseWriter, r *http.Request) {
 	returnView(w, r, "postCreateUpdateForm.html", data)
 }
 
-func (c *PostController) UpdatePostPut(w http.ResponseWriter, r *http.Request) {
+func (c *PostController) UpdatePostPost(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.FormValue("id"))
 	if err != nil {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
@@ -78,25 +80,48 @@ func (c *PostController) UpdatePostPut(w http.ResponseWriter, r *http.Request) {
 
 	if post.UserID != user.ID {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		//return
 	}
 
 	title := r.FormValue("title")
 	content := r.FormValue("content")
+	forSubcribers := r.FormValue("forSubcribers")
+
+	// TO-DO: Validar los datos del formulario
 
 	post.Title = title
 	post.Content = content
+	post.ForSubcribers = forSubcribers == "on"
 
 	err = post.UpdatePost()
 	if err != nil {
 		data := make(map[string]interface{})
 		data["Post"] = post
 		data["Errors"] = PostFormError{
-			Title:   "Check if the title is correct",
-			Content: "Check if the content is correct",
+			Title:         "Check if the title is correct",
+			Content:       "Check if the content is correct",
+			ForSubcribers: forSubcribers == "on",
 		}
 
 		returnView(w, r, "postCreateUpdateForm.html", data)
+	}
+
+	http.Redirect(w, r, "/profile", http.StatusSeeOther)
+}
+
+func (c *PostController) DeletePostGet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+	}
+
+	user := r.Context().Value(shared.AUTH_USER).(models.User)
+
+	post := models.Post{}
+	post.GetPostByID(id)
+	post.DeletePost()
+
+	if post.UserID != user.ID {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	}
 
 	http.Redirect(w, r, "/profile", http.StatusSeeOther)
